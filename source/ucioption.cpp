f63818f54bd4a348c672215d62ffe717a43b7033
kozlov-sergey-aleksandrovitch <kozlov.sergey.aleksandrovitch@gmail.com>
2018-03-18 13:32:52 +0300

    Junoir Options Added.

--- source/ucioption.cpp
+++ source/ucioption.cpp
@@ -90,7 +90,13 @@ void init(OptionsMap& o) {
   o["Minimum Thinking Time"] << Option(20, 0, 5000);
   o["Slow Mover"]            << Option(84, 10, 1000);
   o["nodestime"]             << Option(0, 0, 10000);
-  o["Junior Depth"]			 << Option(MAX_PLY-1, 1, MAX_PLY-1);
+  o["Junior Depth"]					<< Option(MAX_PLY-1, 1, MAX_PLY-1);
+  o["Junior Mobility"]				<< Option(true);
+  o["Junior King"]					<< Option(true);
+  o["Junior Threats"]				<< Option(true);
+  o["Junior Passed"]				<< Option(true);
+  o["Junior Space"]					<< Option(true);
+  o["Junior Initiative"]			<< Option(true);
   o["NeverClearHash"]           << Option(false);
   o["HashFile"]                 << Option("SugaR_hash.hsh", on_HashFile);
   o["SaveHashtoFile"]           << Option(SaveHashtoFile);
