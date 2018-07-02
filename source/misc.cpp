ddd93fdbc638fd348e86946b8fc7d357ded156d6
Marco Zerbinati <zerbinati.marco@gmail.com>
2018-07-02 15:02:12 +0200

    SugaR XPrO 1.5
    New Official Release

--- source/misc.cpp
+++ source/misc.cpp
@@ -58,7 +58,7 @@ namespace {
 
 /// Version number. If Version is left empty, then compile date in the format
 /// DD-MM-YY and show in engine_info.
-const string Version = "";
+static const string Version = " ";
 
 /// Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 /// cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
@@ -129,7 +129,7 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "S_XPrO " << Version << setfill('0');
+  ss << "SugaR XPrO 1.5" << Version << setfill('0');
 
   if (Version.empty())
   {
